5f938ff65aa65c058cc7af111300c2175e86f8d6
hkpro <1508907793@qq.com>
2017-11-08 10:19:46 +0800

    handle.py 实现图片处理

--- handle.py
+++ handle.py
@@ -35,14 +35,21 @@ class Handle(object):
             # 后台打印日志
             print "Handle Post webdata is ", webData
             recMsg = receive.parse_xml(webData)
-            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == "text":
+            if isinstance(recMsg, receive.Msg):
                 toUser = recMsg.FromUserName
-                fromUser = recMsg.ToUser
-                content = "test"
-                replyMsg = reply.TextMsg(toUser, fromUser, content)
-                return replyMsg.send()
+                fromUser = recMsg.ToUserName
+                if recMsg.MsgType == "text":
+                    content = "test"
+                    replyMsg = reply.TextMsg(toUser, fromUser, content)
+                    return replyMsg.send()
+                if recMsg.MsgType == "image":
+                    mediaId = recMsg.MediaId
+                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+                    return replyMsg.send()
+                else:
+                    return reply.Msg().send()
             else:
                 print "暂且不处理"
-                return "success"
+                return reply.Msg().send()
         except Exception, Argment:
             return Argment
